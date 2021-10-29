cd971feb54e5c659ebc8d1bb4e7e3518c8e9b4e9
chul0721 <chul070721@naver.com>
2021-10-30 02:38:26 +0900

    feat: Class Components

--- App.tsx
+++ App.tsx
@@ -3,50 +3,54 @@ import { View } from 'react-native'
 import axios from 'axios'
 import MapView, { Marker } from 'react-native-maps'
 
-declare global {
-  interface Window {
-    kakao: any
-    map: any
-  }
+interface Props {
+  id: 0,
+  distance: 0,
+  latitude: 37.37631358163734,
+  longitude: 127.1104295264883
 }
 
-export default function App() {
-  const Data = {
-    id: 0,
-    distance: 0,
-    latitude: 37.37631358163734,
-    longitude: 127.1104295264883
+export default class App extends React.Component<{}, any>  {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      id: 0,
+      distance: 0,
+      latitude: 37.37631358163734,
+      longitude: 127.1104295264883
+    }
+  }
+  componentDidMount() {
+    axios.get('http://18.116.239.41:8080/data', {
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    })
+      .then((response: any) => {
+        this.setState(response.data)
+        console.log(response.data)
+      })
   }
 
-  const [data, setData] = useState(Data)
-
-  axios.get('http://18.116.239.41:8080/data', {
-    headers: {
-      'Content-Type': 'text/plain'
+  render() {
+    const mapRegion = {
+      latitude: this.state.latitude,
+      longitude: this.state.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
     }
-  })
-    .then((response: any) => {
-      setData(response.data)
-      console.log(response.data)
-    })
 
-  const [ mapRegion ] = useState({
-    latitude: data.latitude,
-    longitude: data.longitude,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
-  })
-
-  return (
-    <View>
-      <MapView
-        style={{ alignSelf: 'stretch', height: '100%' }}
-        initialRegion={mapRegion}
-      >
-        <Marker
-          coordinate={mapRegion}
-        />
-      </MapView>
-    </View>
-  )
+    return (
+      <View>
+        <MapView
+          style={{ alignSelf: 'stretch', height: '100%' }}
+          initialRegion={mapRegion}
+        >
+          <Marker
+            coordinate={mapRegion}
+          />
+        </MapView>
+      </View>
+    )
+  }
 }
