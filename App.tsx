da8166ca18eacb501aba0fdadce8772e36679ab6
chul0721 <chul070721@naver.com>
2021-10-30 13:06:40 +0900

    feat: functional component agaoin

--- App.tsx
+++ App.tsx
@@ -3,55 +3,42 @@ import { View } from 'react-native'
 import axios from 'axios'
 import MapView, { Marker } from 'react-native-maps'
 
-interface Props {
-  id: 0,
-  distance: 0,
-  latitude: 37.37631358163734,
-  longitude: 127.1104295264883
-}
-
-export default class App extends React.Component<{}, any>  {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      id: 0,
-      distance: 0,
-      latitude: 37.37631358163734,
-      longitude: 127.1104295264883
-    }
+export default function App() {
+  const Data = {
+    id: 0,
+    distance: 0,
+    latitude: 37.37631358163734,
+    longitude: 127.1104295264883
   }
 
-  componentDidMount() {
-    axios.get('http://chul0721.iptime.org:25565/data', {
-      headers: {
-        'Content-Type': 'text/plain'
-      }
+  const [data, setData] = useState(Data)
+
+  axios.get('http://chul0721.iptime.org:25565/data', {
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  })
+    .then((response: any) => {
+      setData(response.data)
     })
-      .then((response: any) => {
-        this.setState(response.data)
-        console.log(response.data)
-      })
-  }
 
-  render() {
-    const mapRegion = {
-      latitude: this.state.latitude,
-      longitude: this.state.longitude,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
-    }
+  const [ mapRegion ] = useState({
+    latitude: data.latitude,
+    longitude: data.longitude,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+  })
 
-    return (
-      <View>
-        <MapView
-          style={{ alignSelf: 'stretch', height: '100%' }}
-          initialRegion={mapRegion}
-        >
-          <Marker
-            coordinate={mapRegion}
-          />
-        </MapView>
-      </View>
-    )
-  }
+  return (
+    <View>
+      <MapView
+        style={{ alignSelf: 'stretch', height: '100%' }}
+        initialRegion={mapRegion}
+      >
+        <Marker
+          coordinate={mapRegion}
+        />
+      </MapView>
+    </View>
+  )
 }
